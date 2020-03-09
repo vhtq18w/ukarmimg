ef848e201a5ea30a8b0c2d360fc91d30eb90513f
Burgess Chang <brs@sdf.org>
2020-03-09 11:41:08 +0800

    remove temp script

--- raspi4/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-$TARGET_ROOTFS_DIR="$(pwd)/build/rootfs"
-mount --types proc /proc $TARGET_ROOTFS_DIR/proc
-mount --rbind /sys $TARGET_ROOTFS_DIR/sys
-mount --make-rslave $TARGET_ROOTFS_DIR/sys
-mount --rbind /dev $TARGET_ROOTFS_DIR/dev
-mount --make-rslave $TARGET_ROOTFS_DIR/dev
